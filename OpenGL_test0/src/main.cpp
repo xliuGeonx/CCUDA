16cfdd18630a9340d285e329179f85882d724772
Xin <xin.liu@geonx>
2018-05-23 14:12:25 +0200

    copy dll files

--- OpenGL_test0/src/main.cpp
+++ OpenGL_test0/src/main.cpp
@@ -1,13 +1,52 @@
-﻿#include <iostream>
-#include "cuda_profiler_api.h"
-#include "GL/freeglut.h"
-#include "GL/glew.h"
+﻿#include <GL/glew.h>
+
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#include <GL/wglew.h>
+#endif
+
+#if defined(__APPLE__) || defined(MACOSX)
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#include <GLUT/glut.h>
+#else
+#include <GL/freeglut.h>
+#endif
+
+#include <iostream>
 
 using namespace std;  
   
 
-int main()  
-{  
-  
-  return 0;  
-}  
+//<<<<<<<<<<<<<<<<<<<<<<< myInit >>>>>>>>>>>>>>>>>>>>
+ void myInit(void)
+ {
+    glClearColor(0.0, 0.0, 0.0, 0.0);      // 设置背景颜色为亮白
+    glColor3f(1.0f, 1.0f, 1.0f);           // 设置绘制颜色为黑
+     glPointSize(4.0);                    //设置点的大小为4*4像素
+    glMatrixMode(GL_PROJECTION);// 设置合适的投影矩阵-以后解释 
+    glLoadIdentity();// 以后解释
+    gluOrtho2D(0.0, 640.0, 0.0, 480.0);// 以后解释
+ };
+//<<<<<<<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>
+// 重绘函数
+void myDisplay(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);     // 清屏幕 
+    glBegin(GL_POINTS);
+        glVertex2i(100, 50);         // 绘制若干个点
+        glVertex2i(100, 130);
+        glVertex2i(150, 130);
+    glEnd();    
+    glFlush();                         // 将所有输出到显示屏上 
+};
+//<<<<<<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>>>>>
+void main(int argc, char **argv)
+{
+    glutInit(&argc, argv);          // 初始化工具包
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // 设置显式模式
+    glutInitWindowSize(640,480);     // 设置窗口大小
+    glutInitWindowPosition(100, 150); // 设置窗口位置
+    glutCreateWindow("my first attempt"); // 打开屏幕窗口
+    glutDisplayFunc(myDisplay);     // 注册绘制函数
+    myInit();                   
+    glutMainLoop();              // 进入主循环
+};
---
+++ OpenGL_test1/src/main.cpp
@@ -0,0 +1,134 @@
+﻿#include <GL/glew.h>
+
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#include <GL/wglew.h>
+#endif
+
+#if defined(__APPLE__) || defined(MACOSX)
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#include <GLUT/glut.h>
+#else
+#include <GL/freeglut.h>
+#endif
+
+#include <iostream>
+
+using namespace std;  
+
+#define WINDOW_TITLE_PREFIX "Chapter 1"
+
+int
+  CurrentWidth = 800,
+  CurrentHeight = 600,
+  WindowHandle = 0;
+
+unsigned int FPS = 0;
+
+void Initialize(int, char*[]);
+void InitWindow(int, char*[]);
+void ResizeFunction(int, int);
+void RenderFunction(void);
+void TimerFunction(int);
+void IdleFunction(void);
+
+int main(int argc, char* argv[])
+{
+  Initialize(argc, argv);
+
+  glutMainLoop();
+  
+  exit(EXIT_SUCCESS);
+};
+
+void Initialize(int argc, char* argv[])
+{
+  
+  InitWindow(argc, argv);
+
+  fprintf(
+    stdout,
+    "INFO: OpenGL Version: %s\n",
+    glGetString(GL_VERSION)
+  );
+
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+};
+
+void InitWindow(int argc, char* argv[])
+{
+   glutInit(&argc, argv);
+
+   GLenum err = glewInit();
+   if (GLEW_OK != err)
+   {
+        printf("GLEW Error: %s\n", glewGetErrorString(err));
+        exit(EXIT_FAILURE);
+   }  
+
+  glutSetOption(
+    GLUT_ACTION_ON_WINDOW_CLOSE,
+    GLUT_ACTION_GLUTMAINLOOP_RETURNS
+  );
+  
+  glutInitWindowSize(CurrentWidth, CurrentHeight);
+
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+
+  WindowHandle = glutCreateWindow(WINDOW_TITLE_PREFIX);
+
+  if(WindowHandle < 1) {
+    fprintf(
+      stderr,
+      "ERROR: Could not create a new rendering window.\n"
+    );
+    exit(EXIT_FAILURE);
+  }
+
+  glutReshapeFunc(ResizeFunction);
+  glutDisplayFunc(RenderFunction);
+  glutIdleFunc(IdleFunction);
+  glutTimerFunc(0, TimerFunction, 0);
+};
+
+void ResizeFunction(int Width, int Height)
+{
+  CurrentWidth = Width;
+  CurrentHeight = Height;
+  glViewport(0, 0, CurrentWidth, CurrentHeight);
+};
+
+void RenderFunction(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  ++FPS;
+  glutSwapBuffers();
+  glutPostRedisplay();
+};
+
+void IdleFunction(void)
+{
+  glutPostRedisplay();
+};
+
+void TimerFunction(int Value)
+{
+  if (0 != Value) {
+    char* TempString = (char*)
+      malloc(512 + strlen(WINDOW_TITLE_PREFIX));
+
+    sprintf(
+      TempString,
+      "%s: %d Frames Per Second @ %d x %d",
+      WINDOW_TITLE_PREFIX,
+      FPS * 4,
+      CurrentWidth,
+      CurrentHeight
+    );
+
+    glutSetWindowTitle(TempString);
+    free(TempString);
+  }
+  
+  FPS = 0;
+  glutTimerFunc(250, TimerFunction, 1);
+};
